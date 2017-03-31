715667793c9c0c9172e9090e67ab12c00d871157
wcsngonion <1010136561@qq.com>
2017-03-31 09:58:35 +0800

    demo

---
+++ my/app/src/main/java/com/example/dusicong/my/file_access_RW.java
@@ -0,0 +1,28 @@
+package com.example.dusicong.my;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Du Sicong on 3/30/2017.
+ */
+
+public class file_access_RW extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editname;
+    private EditText editdetail;
+    private Button btnsave;
+    private Button btnclean;
+    private Button btnread;
+    private Context mContext;
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
